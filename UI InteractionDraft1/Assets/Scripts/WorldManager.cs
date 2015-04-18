befe2b867aa91fd9cab2fedc2add7c45215da3c6
mattwehner <mattwehnernz@gmail.com>
2015-04-18 12:51:12 +1200

    Updated Missing Scripts

--- UI InteractionDraft1/Assets/Scripts/WorldManager.cs
+++ UI InteractionDraft1/Assets/Scripts/WorldManager.cs
@@ -1,87 +1,210 @@
-﻿using UnityEngine;
+﻿using Leap;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using System.Collections;
-using Leap;
 
-internal class WorldManager : MonoBehaviour {
-	Controller controller;
-	public Text handModeDisplay;
-	public GameObject camera;
+namespace Assets.Scripts
+{
+    public class WorldManager : MonoBehaviour
+    {
+        public Controller Controller;
+        private bool _isPaused = false;
+        private string _handMode;
+        private int _previousExtendedCount;
+        private string _previousHandMode;
+        private object _previousHandPositionX;
+        private object _previousHandPositionY;
+        public float CameraSpeed;
+        public Text HandModeDisplay;
+        public GameObject MenuCursor;
+        public GameObject Menu;
+        public GameObject InGameUI;
+        public GameObject Player;
+        public GameObject PlayerHands;
+        public GameObject Character;
+        public Button SpawnButton;
 
-	private string handMode;
-	private string previousHandMode;
-	private int previousExtendedCount;
+        private void Start()
+        {
+            Controller = new Controller();
+            Controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+            Controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            _previousExtendedCount = 0;
+            HandModeDisplay.text = "";
+            CameraSpeed = 0.03f;
+            Cursor.visible = false;
+        }
 
-	private object previousHandPositionX;
-	private object previousHandPositionY;
+        
+        private void Update()
+        {
+            var frame = Controller.Frame();
+            var interactionBox = frame.InteractionBox;
+            var hand = frame.Hands[0];
+            var playerPosition = Player.transform.position;
 
-	// Use this for initialization
+            setObjectVisibility();
 
-	void Start () {
-		controller = new Controller();
-		previousExtendedCount = 0;
-		handModeDisplay.text = "";
-	}
+            if (Input.GetKeyDown(KeyCode.Escape))ToggleMenu();
 
-	void Update () {
-		Frame frame = controller.Frame();
-		Hand hand = frame.Hands[0];
+            if (_isPaused || _handMode == "pointing")
+            {
+                MenuController(interactionBox, hand);
+            }
 
-		if (frame.Hands.Count == 0) {
-			handModeDisplay.text = "Place your hand into the scene";
-			return;
-		}
+            if (frame.Hands.Count == 0)
+            {
+                HandModeDisplay.text = "Place your hand into the scene";
+                return;
+            }
+            
 
-		HandModeCalculator (hand);
+            HandModeCalculator(hand);
 
-		if (previousHandMode != handMode) {
-			handModeDisplay.text = "You are " + handMode;
-			Debug.Log ("Hand Mode is " + handMode);
-			previousHandMode = handMode;
-		}
+            if (_previousHandMode != _handMode)
+            {
+                HandModeDisplay.text = "You are " + _handMode;
+                Debug.Log("Hand Mode is " + _handMode);
+                _previousHandMode = _handMode;
+            }
 
-		if (handMode == "camera") {
-			cameraController(hand);
-		}
+            if (_handMode == "camera")
+            {
+                CameraController(interactionBox, hand);
+                if (playerPosition.x < -14)
+                {
+                    playerPosition.x = -13.99f;
+                }
+                if (playerPosition.x > 14)
+                {
+                    playerPosition.x = 13.99f;
+                }
+                if (playerPosition.y < -2)
+                {
+                    playerPosition.y = -1.99f;
+                }
+                if (playerPosition.y > 2)
+                {
+                    playerPosition.y = 1.99f;
+                }
+            }
+        }
+
+        private void setObjectVisibility()
+        {
+            Menu.SetActive(_isPaused);
+            InGameUI.SetActive(!_isPaused);
+            PlayerHands.SetActive(!_isPaused);
+
+            Time.timeScale = _isPaused ? 0 : 1;
+        }
+
+        private void HandModeCalculator(Hand hand)
+        {
+            var fingers = hand.Fingers;
+            var extendedFingers = fingers.Extended();
+
+            var indexFinger = fingers.FingerType(Finger.FingerType.TYPE_INDEX);
+            var thumb = fingers.FingerType(Finger.FingerType.TYPE_THUMB);
+            var pinkyFinger = fingers.FingerType(Finger.FingerType.TYPE_PINKY);
+
+            var isPointing = extendedFingers.Count == 1 &&
+                             extendedFingers[0].Equals(indexFinger[0]);
+
+            var isCamera = extendedFingers.Count == 2 &&
+                           extendedFingers[0].Equals(thumb[0]) &&
+                           extendedFingers[1].Equals(pinkyFinger[0]);
+
+            if (extendedFingers.Count != _previousExtendedCount)
+            {
+                Debug.Log("Extended Fingers Count: " + extendedFingers.Count);
+                _previousExtendedCount = extendedFingers.Count;
+            }
+            if (isPointing)
+            {
+                _handMode = "pointing";
+                _isPaused = true;
+                return;
+            }
+            _isPaused = false;
+
+            if (isCamera)
+            {
+                _handMode = "camera";
+                return;
+            }
+            _handMode = "grabbing";
+        }
+
+        private void CameraController(InteractionBox interactionBox, Hand hand)
+        {
+            var currentPosition = Player.transform.position;
+            var handPosition = interactionBox.NormalizePoint(hand.StabilizedPalmPosition);
+            var moveXAmount = 0f;
+            var moveYAmount = 0f;
+            const float cameraDeadZoneMinX = 0.48f;
+            const float cameraDeadZoneMaxX = 0.52f;
+            const float cameraDeadZoneMinY = 0.68f;
+            const float cameraDeadZoneMaxY = 0.78f;
 
 
-	}
+            if (handPosition.x > cameraDeadZoneMinX && handPosition.x < cameraDeadZoneMaxX)
+            {
+                moveXAmount = 0;
+            }
+            if (handPosition.x < cameraDeadZoneMinX)
+            {
+                moveXAmount = (float) (handPosition.x - 0.5);
+            }
+            if (handPosition.x > cameraDeadZoneMaxX)
+            {
+                moveXAmount = (float) (handPosition.x - 0.5);
+            }
+            if (handPosition.y > cameraDeadZoneMinY && handPosition.y < cameraDeadZoneMaxY)
+            {
+                moveYAmount = 0;
+            }
+            if (handPosition.y < cameraDeadZoneMinY)
+            {
+                moveYAmount = (float) (handPosition.y - 0.5);
+            }
+            if (handPosition.y > cameraDeadZoneMaxY)
+            {
+                moveYAmount = (float) (handPosition.y - 0.5);
+            }
 
-	void HandModeCalculator (Hand hand){
-		FingerList fingers = hand.Fingers;
-		FingerList extendedFingers = fingers.Extended ();
+            var newDestination = new Vector3(Mathf.Clamp((Player.transform.position.x + moveXAmount), -14f, 14f),
+                Mathf.Clamp((Player.transform.position.y + moveYAmount), -2f, 2f), 0);
 
-		FingerList indexFinger = fingers.FingerType(Finger.FingerType.TYPE_INDEX);
-		FingerList thumb = fingers.FingerType(Finger.FingerType.TYPE_THUMB);
-		FingerList pinkyFinger = fingers.FingerType(Finger.FingerType.TYPE_PINKY);
+            Player.transform.position = Vector3.Lerp(currentPosition, newDestination, 1);
+        }
 
-		bool isPointing = extendedFingers.Count == 1 &&
-			extendedFingers [0].Equals (indexFinger [0]);
+        private void MenuController(InteractionBox interactionBox, Hand hand)
+        {
 
-		bool isCamera = extendedFingers.Count == 2 &&
-			extendedFingers [0].Equals (thumb[0]) &&
-				extendedFingers [1].Equals (pinkyFinger[0]);
+            Pointable pointerFinger = hand.Fingers.Frontmost;
 
-		if (extendedFingers.Count != previousExtendedCount) {
-			Debug.Log ("Extended Fingers Count: " + extendedFingers.Count);
-			previousExtendedCount = extendedFingers.Count;
-		}
+            var width = (UnityEngine.Screen.width*1.5f) - (UnityEngine.Screen.width/2);
+            var height = (UnityEngine.Screen.height * 1.5f) - (UnityEngine.Screen.height / 2);
 
-		if (isPointing) {
-			handMode = "pointing";
-			return;
-		}
-		if(isCamera){
-			handMode = "camera";
-			return;
-		}
-			handMode = "grabbing";
-	}
+            Vector leapFingerPosition = interactionBox.NormalizePoint(pointerFinger.StabilizedTipPosition);
+            Vector3 unityFingerPosition = new Vector3(((leapFingerPosition.x) * width), (leapFingerPosition.y * height), -10);
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10);
+            
+            MenuCursor.transform.position = (hand.ToString() == "Invalid Hand")? mousePosition:unityFingerPosition;
+        }
 
-	void cameraController(Hand hand){
-		var moveX = 1;
-		var moveY = 1;
-		Vector handPosition = hand.PalmPosition;
-		camera.transform.Translate(moveX,moveY, 0);
-	}
+        public void SpawnSphere()
+        {
+            var defaultColor = SpawnButton.image.color;
+            SpawnButton.image.color = Color.blue;
+            GameObject.Instantiate(Character, new Vector3(0, 2, 6), Quaternion.identity);
+            SpawnButton.image.color = defaultColor;
+        }
+
+        private void ToggleMenu()
+        {
+            _isPaused = !_isPaused;
+        }
+    }
 }
--- UI InteractionDraft1/Assets/Scripts/cameraController.cs
+++ UI InteractionDraft1/Assets/Scripts/cameraController.cs
@@ -5,13 +5,12 @@ using Leap;
 public class cameraController : MonoBehaviour {
 	Controller controller;
 	private string handMode;
-	public int left;
-	public int height;
-	public int multiplyFactor;
+	public float cameraSpeed;
 
 	// Use this for initialization
 	void Start () {
 		controller = new Controller ();
+		cameraSpeed = 0.03f;
 	}
 	
 	// Update is called once per frame
@@ -20,7 +19,7 @@ public class cameraController : MonoBehaviour {
 		Hand hand = frame.Hands[0];
 
 		InteractionBox interactionBox = frame.InteractionBox;
-		Vector handPosition = interactionBox.NormalizePoint(hand.PalmPosition);
+		Vector handPosition = interactionBox.NormalizePoint(hand.StabilizedPalmPosition);
 
 		FingerList fingers = hand.Fingers;
 		FingerList extendedFingers = fingers.Extended ();
@@ -31,7 +30,18 @@ public class cameraController : MonoBehaviour {
 				extendedFingers [1].Equals (pinkyFinger[0]);
 		
 		if (isCamera) {
-			Debug.Log("camera position: " + handPosition);
+			var cameraPosition = transform.position;
+			var moveX = (handPosition.x <= 0.5)? -cameraSpeed : cameraSpeed;
+			var moveY = (handPosition.y <= 0.75)? -cameraSpeed : cameraSpeed;
+
+
+
+			transform.Translate(moveX,moveY, 0);
+
+			//x: 0, 0.5, 1
+			//y: is 0.5, 0.75, 1
+			Debug.Log("camera position X: " + handPosition.x + " camera position y: " + handPosition.y);
+			//Debug.Log("camera position: " + handPosition);
 		}
 	}
 }
