3eb4eecda759c766593935018500f3b5693b516f
mattwehner <mattwehnernz@gmail.com>
2015-04-19 12:45:25 +1200

    Draft1

--- UI InteractionDraft1/Assets/Scripts/CursorController.cs
+++ UI InteractionDraft1/Assets/Scripts/CursorController.cs
@@ -24,7 +24,7 @@ namespace Assets.Scripts
                 }
             }
         }
-
+        
         private bool hasClicked(Frame frame)
         {
             GestureList gesture = frame.Gestures();
